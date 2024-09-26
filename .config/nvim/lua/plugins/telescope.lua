4fb4aa31d162f1e63d9b0d77951dfe52961b34ba
Kevin Grandjean <moi@kevingrandjean.ch>
2024-09-26 20:55:13 +0200

    Update telescope to show hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,13 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
 
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            pickers = {
+                find_files = {
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
+                }
+            }
+        })
         local builtin = require("telescope.builtin")
         vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = "show project files" })
         vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "show git files" })
