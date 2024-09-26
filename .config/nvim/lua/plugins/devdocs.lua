7ce95baa21588eefe2ee7ecc5d1a728d62290571
Kevin Grandjean <moi@kevingrandjean.ch>
2024-09-26 10:06:22 +0200

    Make glow optional

--- .config/nvim/lua/plugins/devdocs.lua
+++ .config/nvim/lua/plugins/devdocs.lua
@@ -1,25 +1,36 @@
+local function is_glow_available()
+    local handle = io.popen("which glow 2>/dev/null")
+    local result = handle:read("*a")
+    handle:close()
+
+    return result and result ~= ""
+end
+
 return {
     "luckasRanarison/nvim-devdocs",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope.nvim",
         "nvim-treesitter/nvim-treesitter",
-        "ellisonleao/glow.nvim",
+        { "ellisonleao/glow.nvim", enabled = is_glow_available() },
     },
     config = function()
         local devdocs = require("nvim-devdocs")
-        devdocs.setup({
-            previewer_cmd = "glow",
+        local devdocs_config = {
             float_win = {
                 relative = "editor",
                 height = 50,
                 width = 120,
                 border = "rounded",
             },
-            cmd_args = {
+        }
+        if is_glow_available then
+            devdocs_config["previewer_cmd"] = "glow"
+            devdocs_config["cmd_args"] = {
                 "-s", "dark", "-w", "80"
             }
-        })
+        end
+        devdocs.setup(devdocs_config)
 
         vim.keymap.set("n", "<leader>Df", function() devdocs.open_doc_float({ fargs = {} }) end,
             { desc = "open documentation for current file" })
