37b427e470210d6dfb121e7a808e6aeec7841a19
Kevin Grandjean <moi@kevingrandjean.ch>
2024-09-30 22:21:53 +0200

    Follow current file

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -16,7 +16,10 @@ return {
         filesystem = {
             filtered_items = {
                 hide_dotfiles = false,
-            }
-        }
+            },
+            follow_current_file = {
+                enabled = true,
+            },
+        },
     }
 }
